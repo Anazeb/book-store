3fb04f83c301fe5bec12cc51d78c81fc1d111003
Anazeb <anam.aurangzeb@gmail.com>
2023-08-18 23:53:46 +0200

    handle search

--- main.js
+++ main.js
@@ -46,3 +46,105 @@ function updateFilterOptions() {
     <option value="600+">600+</option>
   `;
 }
+
+function handleSearch() {
+  const searchQuery = document.getElementById('search-bar').value.trim().toLowerCase();
+  const bookList = document.getElementById('book-list');
+  if (searchQuery === '') {
+    // If the search query is empty, reset the displayed books to all books
+    filteredBooks = [...books];
+  } else {
+    console.log(searchQuery)
+    // Filter the books based on the search query
+    filteredBooks = books.filter((book) =>
+      book.title.toLowerCase().includes(searchQuery) ||
+      book.author.toLowerCase().includes(searchQuery) ||
+      book.category.toLowerCase().includes(searchQuery)
+    );
+    console.log(filteredBooks)
+    // Modify the function to include the "Buy" button
+    filteredBooks.forEach((book) => {
+      const bookCard = `
+    <div class="col-lg-4 mb-4">
+      <div class="card">
+        <img src="${book.image}" >
+        <div class="card-body">
+          <h5 class="card-title">${book.title}</h5>
+          <p class="card-text">Price: SEK ${book.price}</p>
+          <button class="btn btn-primary" onclick="handleBuy(${book.id})">Buy</button>
+        </div>
+      </div>
+    </div>
+  `;
+      bookList.innerHTML += bookCard;
+    });
+
+  }
+  displayBooks(filteredBooks);
+}
+
+
+function sortBooks(books) {
+  console.log(sortOption)
+  console.log(sortDirection)
+  return books.sort((a, b) => {
+    if (sortOption === 'price') {
+      return sortDirection === 'asc' ? a.price - b.price : b.price - a.price;
+    } else if (sortOption === 'title') {
+      return sortDirection === 'asc' ? a.title.localeCompare(b.title, undefined, { sensitivity: 'base' }) : b.title.localeCompare(a.title, undefined, { sensitivity: 'base' });
+    } else if (sortOption === 'author') {
+      return sortDirection === 'asc' ? a.author.localeCompare(b.author, undefined, { sensitivity: 'base' }) : b.author.localeCompare(a.author, undefined, { sensitivity: 'base' });
+    } else {
+      // Default to sort by title 
+      return a.title.localeCompare(b.title, undefined, { sensitivity: 'base' });
+    }
+  });
+}
+
+// Modify the displayBooks function to use the sortBooks function
+function displayBooks(books) {
+  const bookList = document.getElementById('book-list');
+  bookList.innerHTML = '';
+  console.log(sortDirection)
+
+  // Filter the books based on the selected filters
+  let filteredBooks = books.filter((book) => {
+    const categoryMatch = selectedCategory === 'All' || book.category === selectedCategory;
+    const authorMatch = selectedAuthor === 'All' || book.author === selectedAuthor;
+    const priceMatch = selectedPriceRange === 'All' || (book.price >= Number(selectedPriceRange.split('-')[0]) && book.price <= Number(selectedPriceRange.split('-')[1]));
+
+    return categoryMatch && authorMatch && priceMatch;
+  });
+  console.log(filteredBooks)
+  // Sort the filtered books
+  filteredBooks = sortBooks(filteredBooks);
+  console.log(filteredBooks)
+
+
+  // Render the filtered and sorted books
+  filteredBooks.forEach((book) => {
+    const bookCard = `
+      <div class="col-lg-4 mb-4">
+        <div class="card" onclick="displayBookDetails(${book.id})">
+          <img src="${book.image}" >  
+          <div class="card-body">
+            <h5 class="card-title">${book.title}</h5>
+            <p class="card-text">Price: SEK ${book.price}</p>
+            <p class="card-text">Author: ${book.author}</p>
+          </div>
+        </div>
+        <div class="button-container">
+  <div class="details-button">
+    <button class="btn btn-primary" onclick="displayBookDetails(${book.id})">Details</button>
+  </div>
+  <div class="buy-button">
+    <button class="btn btn-primary" onclick="handleBuy(${book.id})">Buy</button>
+  </div>
+</div>
+
+      </div>
+    `;
+    bookList.innerHTML += bookCard;
+  });
+}
+
